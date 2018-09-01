1f32ee58707a1bd470764f250d208bcc7b37588a
Yago Estevez <inbox@yagoestevez.com>
2018-09-01 18:50:13 +0200

    Add API entrypoints

--- App/routes.js
+++ App/routes.js
@@ -6,8 +6,25 @@ const router  = express.Router();
 const ROOT    = '/';
 
 // View main entrypoint.
-router.get( ROOT, ( req,res ) => {
-  res.text( 'ROOT DIRECTORY' );
+router.get( '/', ( req,res ) => {
+  res.send( 'ROOT DIRECTORY' );
+} );
+
+// API entrypoints.
+router.get( '/users', ( req,res ) => {
+  res.send( 'GET USERS' );
+} );
+
+router.get( '/log', ( req,res ) => {
+  res.send( 'GET LOG' );
+} );
+
+router.post( '/new-user', ( req,res ) => {
+  res.send( 'POST NEW USER' );
+} );
+
+router.post( '/add', ( req,res ) => {
+  res.send( 'POST ADD' );
 } );
 
 module.exports = router;
