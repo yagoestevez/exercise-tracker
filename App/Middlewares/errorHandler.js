0aee7c2163ba216e3e35b90fb4b6c28e8ca48430
Yago Estevez <inbox@yagoestevez.com>
2018-09-02 02:32:47 +0200

    Fix errorHandler middleware

--- App/Middlewares/errorHandler.js
+++ App/Middlewares/errorHandler.js
@@ -1,14 +1,17 @@
 'use strict';
 
 module.exports = ( error, req, res, next ) => {
-  if ( error.code === 400   )
+  if ( error.code === 400 && error.text )
     return res.status( error.code ).send( `ERROR 400: BAD REQUEST.\n${error.text}` );
-  if ( error.code === 404   )
+  if ( error.code === 404 && error.text )
     return res.status( error.code ).send( `ERROR 404: NOT FOUND.\n${error.text}` );
-  if ( error.code === 500   )
+  if ( error.code === 500 && error.text )
     return res.status( error.code ).send( `ERROR 500: INTERNAL SERVER ERROR.\n${error.text}` );
   if ( error.code === 11000 ) {
     return res.send( `ERROR: Sorry, the username is already registered in the system.` );
+  }
+  if ( error.message ) {
+    return res.send( `ERROR 400: BAD REQUEST.\n${error.message}` );
   }
   if ( error.name === 'CastError' )
     return res.status( 500 ).send( 'ERROR:\nSomething went wrong' );
